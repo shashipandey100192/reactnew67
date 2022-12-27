d3a434bf9f12f25ffea8d16a5205baa52d83e63d
windows <windows@microsoft.com>
2022-12-27 18:38:09 +0530

    type any message

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Myclass extends React.Component{
+
+    render()
+    {
+        return(
+            <>
+            <h1>this is class components</h1>
+            <h2>this is sub heading of class components</h2>
+            </>
+        )
+    }
+}
+export default Myclass
---
+++ src/components/Myhome.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from "react";
+
+function Home()
+{
+    const a=500;
+    const b=800;
+    const c="rohit";
+    // return 5000
+    return(
+        <Fragment>
+            <h1>welcome {10+20}</h1>
+            <h1>welcome {a}</h1>
+            <h2>heading {a+b} {a} and {b} and {c}</h2>
+        </Fragment>
+    )
+
+}
+
+export default Home
