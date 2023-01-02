454c7efd1c45fa622df91989864da6df7f2d9755
windows <windows@microsoft.com>
2023-01-02 19:02:23 +0530

    install all ui

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Mybootstrappage from './bootstrapUI/BootstrapHome';
+import Materialuihome from './metrialUI/Materialuihome';
+import Reactbootstraphome from './reactbootstrapUI/Reactbootstraphome';
+
 
 
 
@@ -8,6 +11,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Mybootstrappage/>
+    <h1>react bootstrap</h1>
+    <Reactbootstraphome />
+    <h1>material ui </h1>
+    <Materialuihome/>
   </React.StrictMode>
 );
 
